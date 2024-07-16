df918c66627e61033847193728c2523e27447529
BellCube Dev <33764825+BellCubeDev@users.noreply.github.com>
2024-07-16 13:30:36 -0400

    Use <P> for TextLink
    (instead of the unstyled-by-default React Native <Text>)

--- link/text-link.tsx
+++ link/text-link.tsx
@@ -1,16 +1,16 @@
 'use client'
-import { TextProps, Text } from 'react-native'
+import { P } from 'dripsy'
 
 import { LinkCore } from './core'
 import { LinkCoreProps } from './LinkCoreProps'
 
-type TextLinkProps = LinkCoreProps & { textProps?: TextProps }
+type TextLinkProps = LinkCoreProps & { textProps?: Parameters<typeof P>[0] }
 
 function TextLink({ textProps, ...props }: TextLinkProps) {
   return (
     <LinkCore
       {...props}
-      Component={Text}
+      Component={P as any} // react-native vs react ReactNode, smh
       componentProps={{ selectable: false, ...textProps }}
     />
   )
