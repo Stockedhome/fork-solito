6b4d11775e556f1d462cf84b2b6726cc42ff90c1
BellCube <33764825+BellCubeDev@users.noreply.github.com>
2024-10-09 20:00:29 -0400

    Bug Fix & Remove Files Field from package.json

--- app/navigation/use-pathname.ts
+++ app/navigation/use-pathname.ts
@@ -1,14 +1,5 @@
-import { Platform } from 'react-native'
-import useNextPathname from './use-next-pathname'
-import { useRoute } from '../../params/use-route'
+import { usePathname as usePathnameExpo } from 'expo-router';
 
-// TODO test this with react navigation and expo router. does it work?
 export function usePathname() {
-  if (Platform.OS === 'web') {
-    return useNextPathname()
-  }
-
-  const path = useRoute()?.path
-
-  return path?.includes('?') ? path.split('?')[0] : path
+  return usePathnameExpo()
 }
---
+++ app/navigation/use-pathname.web.ts
@@ -0,0 +1,5 @@
+import useNextPathname from './use-next-pathname'
+
+export function usePathname() {
+  return useNextPathname()
+}
